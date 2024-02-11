6b2cf32d12a41b6e477533a5d9414af8f776b1cc
yukikayuki <yukikayuki@gmail.com>
2024-02-11 19:21:35 +0900

    feat: add not found page and error page

---
+++ app/routes/_error.tsx
@@ -0,0 +1,8 @@
+// app/routes/_error.ts
+import { ErrorHandler } from 'hono'
+
+const handler: ErrorHandler = (e, c) => {
+  return c.render(<h1>Error! {e.message}</h1>)
+}
+
+export default handler
